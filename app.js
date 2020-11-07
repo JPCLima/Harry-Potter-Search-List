ddf67e2de0c211207092b50bb93420e2fa2ab6b9
Joao Lima <joaoplima@ua.pt>
2020-11-07 16:40:28 +0100

    Search by name or house

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 // Get the element charactersList from html
 const charactersList = document.getElementById('charactersList');
-
-
+const searchBar = document.getElementById('searchBar');
 let hpCharacters = [];
+
+// Add a event when typing
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        const regex = new RegExp(`^${searchString}`, 'gi');
+        return character.name.match(regex) || character.house.match(regex);
+    });
+    displayCharacters(filteredCharacters);
+});
 
 
 // Get all the data from Harry Potter API
 const loadCharacters = async () => {
     try {
         const res = await axios.get('https://hp-api.herokuapp.com/api/characters');
-        displayCharacters(res.data);
+        // Store all the data in hpCharacters
+        hpCharacters = await res.data;
+        // Display all the characters
+        displayCharacters(hpCharacters);
     } catch (err) {
         console.error(err);
     }
