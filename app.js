60ff23d9203d355ec5614f574f690a5b13434d92
Joao Lima <joaoplima@ua.pt>
2020-11-07 15:02:11 +0100

    Get data from API

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+// Get the element charactersList from html
+const charactersList = document.getElementById('charactersList');
+let hpCharacters = [];
 
+
+// Get all the data from Harry Potter API
+const loadCharacters = async () => {
+    try {
+        const res = await axios.get('https://hp-api.herokuapp.com/api/characters');
+        displayCharacters(res.data);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+// Function returs a card for the HP character
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+// Load Characters
+loadCharacters();
